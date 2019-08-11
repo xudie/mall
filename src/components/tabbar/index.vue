84e2b51ff8df0ddc38b58c8e9ca4374f1d3b0ec2
xudie <smileoto@163.com>
2019-08-12 01:53:23 +0800

    弃用 svg 改用字体文件

--- src/components/tabbar/index.vue
+++ src/components/tabbar/index.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="g-tabbar">
     <router-link class="g-tabbar-item" to="/home">
-      <img src="@/assets/icons/home.svg" />
+      <i class="iconfont icon-home"></i>
       <span>首页</span>
     </router-link>
-
-    <router-link class="g-tabbar-item" to="/home">
-      <img src="@/assets/icons/appstore.svg" />
-      <span>分类</span>
+    <router-link class="g-tabbar-item" to="/category">
+      <i class="iconfont icon-category"></i>
+      <span>分类页</span>
     </router-link>
-
-    <router-link class="g-tabbar-item" to="/home">
-      <img src="@/assets/icons/shopping.svg" />
+    <router-link class="g-tabbar-item" to="/cart">
+      <i class="iconfont icon-cart"></i>
       <span>购物车</span>
     </router-link>
-
-    <router-link class="g-tabbar-item" to="/home">
-      <img src="@/assets/icons/user.svg" />
-      <span>我的</span>
+    <router-link class="g-tabbar-item" to="/personal">
+      <i class="iconfont icon-personal"></i>
+      <span>个人中心</span>
     </router-link>
   </div>
 </template>
@@ -31,25 +28,4 @@ export default {
 
 <style lang="scss" scoped>
 // scoped:只作用于当前页面
-@import "~@/assets/scss/mixins";
-
-.g-tabbar {
-  width: 100%;
-  display: flex;
-  height: $tabbar-height;
-  background-color: #fff;
-
-  &-item {
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-
-    img {
-      width: $icon-img-size;
-      height: $icon-img-size;
-    }
-  }
-}
 </style>
